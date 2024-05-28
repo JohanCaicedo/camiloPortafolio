5642ee45d470703d94efd2bf2754c712d16482ac
johancamilo1507 <johancamilo1507@gmail.com>
2024-05-28 14:07:57 +0000

    :sparkles: Basic Bento Grid
    Add basic Bento Grid

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				customPurple: '#6a5deb',
+			},
+		},
 	},
 	plugins: [],
 }
