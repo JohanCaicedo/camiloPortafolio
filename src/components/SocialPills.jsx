01bcc79499c035a837b6a922f0e36f9d382cfaff
Camilo <camilo.design07@gmail.com>
2024-05-30 10:58:28 -0500

    💄 Education UI and Fix
    Edicuation complete and dark mode fix

--- src/components/SocialPills.jsx
+++ src/components/SocialPills.jsx
@@ -17,7 +17,7 @@ function SocialPills(props) {
     >
       <button
         type="button"
-        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex items-center gap-x-2 py-3 mt-4"
+        className="text-white bg-[#22282e] dark:bg-[#96acc8] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center me-2 mb-2 flex items-center gap-x-2 mt-4"
       >
         {props.children}
       </button>
