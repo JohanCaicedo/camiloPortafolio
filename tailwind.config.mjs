cfc61d1356d0724a41c53b05e384111fe4d4e612
Camilo <camilo.design07@gmail.com>
2024-06-06 19:42:38 -0500

    🎉 Finish firsh proyect portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {
-			fontFamily: {
-				NotoSerif: ['Noto Serif Variable'],
-			},
-			colors: {
-				customPurple: '#6a5deb',
-			},
+	  extend: {
+		fontFamily: {
+		  NotoSerif: ['Noto Serif Variable'],
+		  SourceSans: ['Source Sans Pro', 'sans-serif'],
+		  Merriweather: ['Merriweather Sans Variable', 'sans-serif'], // ¡Añadido 'sans-serif' aquí!
 		},
+		colors: {
+		  customPurple: '#6a5deb',
+		},
+	  },
 	},
 	plugins: [],
-}
+  };
+  
