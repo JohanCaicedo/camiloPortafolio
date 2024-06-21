b910ee02e7f884246671b1fea62e23f0d69516c0
Camilo <camilo.design07@gmail.com>
2024-06-21 10:12:01 -0500

    🎨 Toast and use in movil

--- src/components/Spline.jsx
+++ src/components/Spline.jsx
@@ -1,10 +1,34 @@
+
+
+import React, { useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
+import { isWindows, isMacOs, isMobile } from 'react-device-detect';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "../styles/global.css";
 
 export default function App() {
+  useEffect(() => {
+    const toastShown = localStorage.getItem('toastShown');
+    
+    if (!toastShown && (isMobile || (!isWindows && !isMacOs))) {
+      toast.warn('Use desktop for better experience', {
+        autoClose: 5000, // 3 seconds
+      });
+      localStorage.setItem('toastShown', 'true');
+    }
+  }, []);
+
+  if (isMobile || (!isWindows && !isMacOs)) {
+    return <ToastContainer />;
+  }
+
   return (
-    <Spline 
-    className='splineContainer shadow-lg'
-    scene="/Hero.splinecode" />
+    <>
+      <Spline 
+        className='splineContainer shadow-lg'
+        scene="/Hero.splinecode" />
+      <ToastContainer />
+    </>
   );
 }
