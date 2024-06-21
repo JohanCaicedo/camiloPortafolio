572605fa281bb2ff2fd7f8e3c107ac5f7ca207e3
Camilo <camilo.design07@gmail.com>
2024-06-20 19:39:22 -0500

    ✨ Hero and navbar
    New hero and navbar. Some adicional feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/styles/global.css";`
+        }
+      }
+    }
 });
---
+++ src/components/Spline.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+import "../styles/global.css";
+
+export default function App() {
+  return (
+    <Spline 
+    className='splineContainer shadow-lg'
+    scene="https://prod.spline.design/dN07KbnGhRl38txY/scene.splinecode" />
+  );
+}
