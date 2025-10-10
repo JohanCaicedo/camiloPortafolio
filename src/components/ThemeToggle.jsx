d19504417b38388721b7f511dfa312af0021f2b9
Camilo <camilo.design07@gmail.com>
2025-10-10 12:11:56 -0500

    📱 Resposive design
    Personal, editorial design resposive and some improvements

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,42 +1,38 @@
 import { useState, useEffect } from 'react';
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    // Initialize darkMode state based on localStorage
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'dark' ? true : false;
+  });
 
   useEffect(() => {
-    // Verificar si estamos en el navegador (window está definido)
-    if (typeof window !== 'undefined') { 
-      const savedTheme = localStorage.getItem("theme");
-      const initialDarkMode = savedTheme === "dark";
-      setDarkMode(initialDarkMode);
-
-      if (initialDarkMode) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
+    // Update the document's class list based on darkMode
+    if (document.readyState === 'complete') {
+      updateTheme();
+    } else {
+      // Add a MutationObserver to handle DOMContentLoaded
+      const observer = new MutationObserver(updateTheme);
+      observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true,
+      });
     }
-  }, []); // El useEffect se ejecuta solo una vez al montar el componente en el cliente
+  }, [darkMode]);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      if (darkMode) {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-      }
+  const updateTheme = () => {
+    if (document.documentElement.classList.contains('dark')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
     }
-  }, [darkMode]); // Este useEffect se ejecuta cuando darkMode cambia
-
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle('dark', darkMode);
   };
 
   return (
-    <button type="button" onClick={toggleTheme}>
-      {darkMode ? "☀️" : "🌙"}
+    <button type="button" onClick={() => updateTheme()}>
+      {darkMode ? '☀️' : '🌙'}
     </button>
   );
 };
